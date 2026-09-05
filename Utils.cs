5e66d301b9885c54c07a506e6933c0792496cc1d
key <cashwoods146@gmail.com>
2026-09-05 06:47:25 -0500

    Clean up Utils.cs by removing comments
    Removed decompiled metadata comments from Utils.cs

--- Utils.cs
+++ Utils.cs
@@ -1,9 +1,3 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: BigscreenBehind.Utils
-// Assembly: BigscreenBehind, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: 8CD1E9EE-0987-4B29-93F8-7443D82AE0EE
-// Assembly location: C:\Users\CASHM\Downloads\BigscreenBehind.dll
-
 using Il2CppBigscreen;
 using Il2CppBigscreen.Cloud;
 using Il2CppBigscreen.UI;
