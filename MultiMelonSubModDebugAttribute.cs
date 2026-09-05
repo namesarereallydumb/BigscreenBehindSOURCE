f55466fe99b141e240ce85904dc5d14b253dffe9
key <cashwoods146@gmail.com>
2026-09-05 06:44:47 -0500

    Clean up decompiled comments in MultiMelonSubModDebugAttribute
    Removed decompiled comments from MultiMelonSubModDebugAttribute.cs

--- MultiMelonSubModDebugAttribute.cs
+++ MultiMelonSubModDebugAttribute.cs
@@ -1,9 +1,3 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: BigscreenBehind.MultiMelonSubModDebugAttribute
-// Assembly: BigscreenBehind, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: 8CD1E9EE-0987-4B29-93F8-7443D82AE0EE
-// Assembly location: C:\Users\CASHM\Downloads\BigscreenBehind.dll
-
 using System;
 
 #nullable disable
