9012499815eb5591f702b12096ff8744523617df
key <cashwoods146@gmail.com>
2026-09-05 06:44:00 -0500

    Clean up LobbyPropsRemover.cs by removing comments
    Removed decompiled comments from LobbyPropsRemover.cs

--- LobbyPropsRemover.cs
+++ LobbyPropsRemover.cs
@@ -1,9 +1,3 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: BigscreenBehind.LobbyPropsRemover
-// Assembly: BigscreenBehind, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: 8CD1E9EE-0987-4B29-93F8-7443D82AE0EE
-// Assembly location: C:\Users\CASHM\Downloads\BigscreenBehind.dll
-
 using MelonLoader;
 using System.Collections.Generic;
 using UnityEngine;
