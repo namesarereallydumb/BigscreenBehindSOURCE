f415c6850524ae89741ea0b6654ef8e0a3eb38be
key <cashwoods146@gmail.com>
2026-09-05 06:46:17 -0500

    Clean up DisableTeleport.cs by removing comments
    Removed decompiled comments from DisableTeleport.cs

--- DisableTeleport.cs
+++ DisableTeleport.cs
@@ -1,9 +1,3 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: BigscreenBehind.DisableTeleport
-// Assembly: BigscreenBehind, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: 8CD1E9EE-0987-4B29-93F8-7443D82AE0EE
-// Assembly location: C:\Users\CASHM\Downloads\BigscreenBehind.dll
-
 using Il2CppBigscreen;
 using MelonLoader;
 using System.Collections;
